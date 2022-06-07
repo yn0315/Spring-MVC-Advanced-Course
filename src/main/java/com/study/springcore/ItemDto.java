2f9c6c6d1228c5146e0ed85248c53f43f9ee14c9
yn0315 <yunu0315@naver.com>
2022-06-07 18:52:49 +0900

    [2022.06.07]

---
+++ src/main/java/com/study/springcore/ItemDto.java
@@ -0,0 +1,11 @@
+package com.study.springcore;
+
+import lombok.Getter;
+
+@Getter
+public class ItemDto {
+    private String title;
+    private String link;
+    private String image;
+    private int lprice;
+}
