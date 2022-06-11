37b6175ed9a80fa2dfb1e6b3d3bb5f66720fa6e6
yn0315 <yunu0315@naver.com>
2022-06-11 22:53:59 +0900

    AllInOneController

---
+++ src/main/java/com/study/springcore/ProductMypriceRequestDto.java
@@ -0,0 +1,12 @@
+package com.study.springcore;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class ProductMypriceRequestDto {
+    private int myprice;
+}
