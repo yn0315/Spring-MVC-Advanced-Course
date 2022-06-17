4f64893148485dd000ab6dbb281d587f93a25628
yn0315 <yunu0315@naver.com>
2022-06-18 00:04:02 +0900

    [2022.06.18]

---
+++ src/main/java/com/study/springcore/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.study.springcore.repository;
+import com.study.springcore.model.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<Users, Long> {
+    Optional<Users> findByUsername(String username);
+}
